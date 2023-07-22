7fc0d505fdd45629b17d207714c74ba440999151
nickolay.grin <grinmail84@gmail.com>
2023-07-22 23:19:42 +0300

    eeewdss

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'VueProject/static',
+    assetsSubDirectory: '/static',
     assetsPublicPath: './',
     proxyTable: {},
 
@@ -42,7 +42,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'VueProject/static',
+    assetsSubDirectory: '/static',
     assetsPublicPath: '/',
 
     /**
