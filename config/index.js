efbc6e1ad5efc4a3e74072aa748a5df30a429a8c
nickolay.grin <grinmail84@gmail.com>
2023-07-23 11:54:19 +0300

    fdsfdsf

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: './static',
+    assetsSubDirectory: '',
     assetsPublicPath: './',
     proxyTable: {},
 
@@ -42,7 +42,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: './static',
+    assetsSubDirectory: '',
     assetsPublicPath: './',
 
     /**
@@ -51,7 +51,7 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map', 
+    devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-  publicPath: 'VueProject'
+  publicPath: '/VueProject/'
 }
